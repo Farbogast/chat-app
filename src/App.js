61d008abe1c1bf896f52722cfc7c153fc9f7c06d
François ARBOGAST <arbogast.francois@gmail.com>
2020-04-01 10:18:42 +0100

    Quest 2.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Contact from "./components/Contact";
+import PropTypes from "prop-types";
+
+const users = [
+  {
+    name: "Elaine Perez",
+    avatar: "https://randomuser.me/api/portraits/women/21.jpg",
+    status: true
+  },
+  {
+    name: "Ted Rodriquez",
+    avatar: "https://randomuser.me/api/portraits/men/24.jpg",
+    status: false
+  },
+  {
+    name: "Joyce Warren",
+    avatar: "https://randomuser.me/api/portraits/women/61.jpg",
+    status: true
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      {users.map(function(user) {
+        return <Contact {...user} />;
+      })}
+      ;
     </div>
   );
 }
+Contact.PropTypes = {
+  userName: PropTypes.string.isRequired,
+  userStatus: PropTypes.bool.isRequired,
+  userAvatar: PropTypes.string.isRequired
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,19 @@
 import React from "react";
 import "./Contact.css";
 
-const name = "Elaine Perez";
-const avatar = "https://randomuser.me/api/portraits/women/21.jpg";
-const online = true;
-
-function Contact() {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={avatar} alt={name} />
-      <div>
-        <h4 className="name">{name}</h4>
-        <div className="status">
-          <span className={online ? "status-online" : "status-offline"}></span>
-          <p className="status-text">{online ? "Online" : "Offline"}</p>
-        </div>
+const Contact = props => (
+  <div className="Contact">
+    <img className="avatar" src={props.avatar} alt={props.name} />
+    <div>
+      <h4 className="name">{props.name}</h4>
+      <div className="status">
+        <span
+          className={props.status ? "status-online" : "status-offline"}
+        ></span>
+        {props.status ? "online" : "offline"}
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default Contact;
