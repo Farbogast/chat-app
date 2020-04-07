f3650ae2997487ea564dbdaa3b56cb0df3895e57
François ARBOGAST <arbogast.francois@gmail.com>
2020-04-07 10:22:12 +0100

    Add click state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,35 @@
 import React from "react";
 import "./Contact.css";
 
-const Contact = props => (
-  <div className="Contact">
-    <img className="avatar" src={props.avatar} alt={props.name} />
-    <div>
-      <h4 className="name">{props.name}</h4>
-      <div className="status">
-        <span
-          className={props.status ? "status-online" : "status-offline"}
-        ></span>
-        {props.status ? "online" : "offline"}
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: props.status
+    };
+  }
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div className="status">
+            <span
+              className={this.state.status ? "status-online" : "status-offline"}
+              onClick={event => {
+                let newState = !this.state.status;
+                this.setState({ status: newState });
+              }}
+            ></span>
+            <p className="status-text">
+              {this.state.status ? "Online" : "Offline"}
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Contact;
